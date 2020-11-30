c7beedcba722e1ed9896bcf79d1b255ca26c698a
Frie <fr1e@pm.me>
2020-11-30 13:53:31 +0100

    #2 add starter data

---
+++ data-raw/example_correlaidx_geo.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(tmaptools)
+library(sf)
+cities <- tibble(chapter = c("Berlin", "Dortmund", "Paris", "Mannheim", "Munich", "Bremen", "Stuttgart", "Hamburg", "Konstanz", "Nederland", "Rhein-Main",
+                             "Cologne/Bonn", "Karlsruhe", "Hannover/Braunschweig", "Leipzig", "Switzerland"),
+                 city_geo = c("Berlin", "Dortmund", "Paris", "Mannheim", "Munich", "Bremen", "Stuttgart", "Hamburg", "Konstanz", "Amsterdam", "Frankfurt",
+                              "Cologne", "Karlsruhe", "Hannover", "Leipzig", "Zurich"),
+                 label = c(2018, 2018, 2019, 2018, 2020, 2018, 2020, 2020, 2018, 2018, 2018, "coming soon?!", "coming soon?!", 2020, "coming soon?!", "coming soon?!"),
+                 year_founded = c(2018, 2018, 2019, 2018, 2020, 2018, 2020, 2020, 2018, 2018, 2018, 2021, 2021, 2020, 2021, 2021))
+
+geocoded <- tmaptools::geocode_OSM(cities$city_geo)
+
+cities <- cities %>% left_join(geocoded, by = c("city_geo" = "query"))
+
+example_correlaidx_geo <-
+  cities %>%
+  st_as_sf(coords = c("lon", "lat"),
+           crs = 4326)
+
+usethis::use_data(example_correlaidx_geo, overwrite = TRUE)
+
+
