bbcaa93a63ed6933820855d9985adfbe214bdd32
Frie <fr1e@pm.me>
2020-11-30 13:50:18 +0100

    #3 add example data

---
+++ data-raw/example_projects_labels.R
@@ -0,0 +1,18 @@
+## code to prepare `DATASET` dataset goes here
+library(gh)
+library(purrr)
+library(dplyr)
+library(tidyr)
+username <- "correlaid"
+
+# requires read access to correlaid/projects
+projects <- gh::gh("GET /repos/correlaid/projects/issues", per_page = 100, state = "all")
+projects_df <- tibble::tibble(number = projects %>% purrr::map_int("number"), state = projects %>% purrr::map_chr("state"))
+projects_df <- projects_df %>%
+  mutate(labels = projects %>% purrr::map("labels"))
+
+example_projects_labels <- tidyr::unnest(projects_df, labels) %>%
+  tidyr::unnest_wider(labels) %>%
+  tidyr::separate(name, c("category", "value"), extra = "merge", fill = "right")
+
+usethis::use_data(example_projects_labels, overwrite = TRUE)
